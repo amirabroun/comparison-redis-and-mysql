dc06787339a84f82ede06bfd4e5892dbe39deba2
Amir Abroun <abroon234@gmail.com>
2024-01-13 20:25:38 +0330

    fix: redis test function

--- public/index.php
+++ public/index.php
@@ -5,10 +5,12 @@ require __DIR__ . '/../vendor/autoload.php';
 $selectedDataBase = 'mysql';
 
 if ($selectedDataBase == 'redis') {
+    $insertTime = insertDataWithRedis();
+
     [$selectTime, $result] = getDataFromRedis();
 
     var_dump([
-        'Run time for set data:' => insertDataWithRedis(),
+        'Run time for set data:' => $insertTime,
         'Run time for get data:' => $selectTime,
         'data' => $result,
     ]);
